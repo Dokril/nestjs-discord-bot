89ee9adb0ca5cc1bc1c027060bef5a1654479556
Dmitriy Petrov <dvp@automama.ru>
2021-03-02 12:36:30 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { RMQModule } from 'nestjs-rmq';
-import { getMongoConnect, getMongoOptions } from './configs/mongo.config';
-import { ConfigService, ConfigModule } from 'nestjs-dotenv';
-import { TypegooseModule } from 'nestjs-typegoose';
+import { ConfigService } from 'nestjs-dotenv';
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 
 const configService = new ConfigService();
 
 @Module({
-	imports: [ConfigModule.forRoot(), TypegooseModule.forRoot(getMongoConnect(configService), getMongoOptions())],
 	providers: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,28 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-
 import { Client } from 'discord.js';
-import { ConfigService } from 'nestjs-dotenv';
 
 @Injectable()
 export class AppService implements OnModuleInit {
-	constructor(private configService: ConfigService) {}
+	constructor() {}
 	logger = new Logger('Discord bot');
 	private readonly client = new Client();
 
 	async onModuleInit() {
-		await this.client.login(this.configService.get('TOKEN'));
+		await this.client.login(
+			'ODE1ODYwMTkyMjc2NjQzODQw.YDyjAw.60LrBDFE8MDFmw5_7rTa-EUa9IU'
+		);
 		this.start();
 	}
 
 	start() {
 		this.client.on('ready', () => {
-			this.logger.log(`Бот запущен на следующих серверах: ${this.client.guilds.cache.array().join(', ')}`);
+			this.logger.log(
+				`Бот запущен на следующих серверах: ${this.client.guilds.cache
+					.array()
+					.join(', ')}`
+			);
 		});
+
 		this.client.on('message', (message) => {
 			switch (message.channel.type) {
 				case 'dm':
