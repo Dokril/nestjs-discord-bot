c34dbe9c761cebba1c42cb9c49e56a9495af09b7
Dmitriy Petrov <dvp@automama.ru>
2021-03-10 11:47:47 +0300

    add discord nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { RMQModule } from 'nestjs-rmq';
-import { ConfigService } from 'nestjs-dotenv';
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-
-const configService = new ConfigService();
+import { DiscordModule } from 'discord-nestjs';
 
 @Module({
+	imports: [
+		DiscordModule.forRoot({
+			token: 'ODE1ODYwMTkyMjc2NjQzODQw.YDyjAw.60LrBDFE8MDFmw5_7rTa-EUa9IU',
+			commandPrefix: '!',
+			allowCommands: [
+				{
+					name: 'random',
+				},
+			],
+		}),
+	],
 	providers: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,45 +1,38 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { Client } from 'discord.js';
-
+import { Injectable, Logger } from '@nestjs/common';
+import { Message, VoiceChannel, VoiceState } from 'discord.js';
+import { DiscordClientProvider, On, Once, OnCommand } from 'discord-nestjs';
+import { randomInteger } from './common-func';
+import * as discordTTS from 'discord-tts';
 @Injectable()
-export class AppService implements OnModuleInit {
-	constructor() {}
-	logger = new Logger('Discord bot');
-	private readonly client = new Client();
+export class AppService {
+	private readonly logger = new Logger(AppService.name);
+	constructor(private readonly discordProvider: DiscordClientProvider) {}
 
-	async onModuleInit() {
-		await this.client.login(
-			'ODE1ODYwMTkyMjc2NjQzODQw.YDyjAw.60LrBDFE8MDFmw5_7rTa-EUa9IU'
+	@Once({ event: 'ready' })
+	onceReady(): void {
+		this.logger.log(
+			`Бот запущен на следующих серверах: ${this.discordProvider.getClient().guilds.cache.array().join(', ')}`
 		);
-		this.start();
 	}
 
-	start() {
-		this.client.on('ready', () => {
-			this.logger.log(
-				`Бот запущен на следующих серверах: ${this.client.guilds.cache
-					.array()
-					.join(', ')}`
-			);
-		});
+	@OnCommand({ name: 'random', channelType: ['text'] })
+	async onCommand(message: Message): Promise<void> {
+		await message.reply(randomInteger(1, 1000));
+	}
 
-		this.client.on('message', (message) => {
-			switch (message.channel.type) {
-				case 'dm':
-					this.logger.log(
-						`Пользователь ${message.author.username} написал: ${message.content}`,
-						'Личное сообщение'
-					);
-					break;
-				case 'text':
-					this.logger.log(
-						`Пользователь ${message.author.username} написал: ${message.content}`,
-						message.guild?.name
-					);
+	@On({ event: 'voiceStateUpdate' })
+	async onVoiceStateUpdate({ member: oldM, channel: oldC }: VoiceState, { member: newM, channel: newC }: VoiceState) {
+		if (!oldC && newC) {
+			this.logger.log('Зашел на ' + newC.name, oldM.user.username);
+			await this.sendVoiceMessage(newC, 'Пришел мой любимый ' + newM.user.username);
+		} else if (!newC) {
+			this.logger.log('Вышел из ' + oldC.name, oldM.user.username);
+		}
+	}
 
-				default:
-					break;
-			}
-		});
+	async sendVoiceMessage(channel: VoiceChannel, message: string) {
+		const connection = await channel.join();
+		const dispacher = connection.play(discordTTS.getVoiceStream(message, 'ru-RU'));
+		dispacher.on('finish', () => channel.leave());
 	}
 }
